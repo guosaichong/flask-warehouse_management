20b31695e68d8070087b76e259c2026507aecb81
guosaichong <510763683@qq.com>
2020-06-14 08:48:42 +0800

    f

--- models.py
+++ models.py
@@ -1,14 +1,15 @@
 from flask_login import UserMixin
 
+
 class User(UserMixin):
     pass
 
 users = [
-    {'id':'Tom', 'username': 'Tom', 'password': '111111'},
-    {'id':'Michael', 'username': 'Michael', 'password': '123456'}
+    {'username': 'Tom', 'password': '111111'},
+    {'username': 'Michael', 'password': '123456'}
 ]
 
-def query_user(user_id):
+def query_user(username):
     for user in users:
-        if user_id == user['id']:
+        if username == user['username']:
             return user
--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, redirect, url_for, render_template,flash
+from flask import Flask, request, redirect, url_for, render_template, flash
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from models import User, query_user
+import os
+import json
 
 app = Flask(__name__)
 app.secret_key = '1234567'
@@ -30,8 +32,8 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user_id = request.form.get('userid')
-        user = query_user(user_id)
+        username = request.form.get('uname')
+        user = query_user(username)
         if user is not None and request.form['password'] == user['password']:
 
             curr_user = User()
@@ -48,11 +50,47 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form.get("uname")
+    password = request.form.get("psw")
+    repet_password = request.form.get("repet_psw")
+    print(username, password, repet_password)
+    if password==repet_password:
+        #保存用户名和密码到数据库
+        pass
+    else:
+        pass
+    return {"success": 0}
+
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return 'Logged out successfully!'
+
+
+def load_from_json():
+    if os.path.exists("db.json"):
+        json_data = open("db.json", "r").read()
+        return json.loads(json_data)
+    else:
+        return []
+
+
+def save_user_to_json(user_info):
+    user_list = load_user()
+    user_list.append(user_info)
+    with open("./users.json", "w") as f:
+        f.write(json.dumps(user_list,ensure_ascii=False,indent=2))
+
+
+def load_user():
+    if os.path.exists("./user.json"):
+        return json.loads(open("./users.json").read())
+    else:
+        return []
 
 
 if __name__ == '__main__':
